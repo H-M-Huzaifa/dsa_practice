b0880fe62eb2aecd60451577bbe90d47fae18cc2
Hafiz Muhammad Huzaifa <muhammadhuzaifa5200@gmail.com>
2025-09-06 16:50:02 +0500

    added another way of Palindrome cheking in d2_T4

--- lib/Daily_Tasks/d2_t4.dart
+++ lib/Daily_Tasks/d2_t4.dart
@@ -14,3 +14,25 @@ void main(){
 
   }
 }
+
+
+//aother way to do it without built-in functions
+// void main(){
+//   String word ="madam";
+//   int start=0;
+//   int end= word.length-1;
+//   bool isPalindrome=true;
+//
+//   while(start<end){
+//     if(word[start]!=word[end]){
+//       isPalindrome=false;
+//     }
+//     start++;
+//     end--;
+//
+//     if(isPalindrome){
+//       print("The word ${word} is a Palindrome");
+//     }
+//   }
+//
+// }/
